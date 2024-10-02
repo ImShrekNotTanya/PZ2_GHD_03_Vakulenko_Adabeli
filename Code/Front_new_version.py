87058b61c3e010c14649eb8746d210d5799432cb
ImShrekNotTanya <dinozavrik.tata@gmail.com>
2024-10-02 19:46:27 +0300

    Новый код front (убрана проверка подключения с кодом back)

---
+++ Code/Front_new_version.py
@@ -0,0 +1,9 @@
+import Back
+from Back import dis_calc
+
+a = int(input("Введите первый аргумент: "))
+b = int(input("Введите второй аргумент: "))
+c = int(input("Введите третий аргумент: "))
+
+d = dis_calc(a, b, c)
+print(d)
